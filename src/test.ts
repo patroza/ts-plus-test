9515f83113c1958dfb435783d57d659e2d71f589
Patrick Roza <contact@patrickroza.com>
2022-03-20 11:58:27 +0100

    cleanup

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,3 @@
-import { Sync } from "@effect-ts/core-next/io-light/Sync"
-
 import { NamePart, Person } from "./Person"
 
 const p = new Person({
@@ -17,4 +15,4 @@ const p3 = p2.rename(NamePart("First 3"), NamePart("Last 3"))
 
 // say hello to each, repeat 3 times
 const program = p.sayHello() > p2.sayHello() > p3.sayHello()
-Sync.collectAll([program, program, program]).run()
+;(program > program > program).run()
