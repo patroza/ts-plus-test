9421744e3c6d41860b11620f370fc38903e53da2
Patrick Roza <contact@patrickroza.com>
2023-01-06 17:23:49 +0100

    Causes error in transformer

--- src/traced.ts
+++ src/traced.ts
@@ -1,9 +1,9 @@
 const program = pipe(
   [1, 2, 3, 4, 5],
-  Effect.forEachPar((n) => (n % 3 === 0 ? Effect.die("boom") : Effect.succeed(n + 1))),
-  Effect.flatMap((chunk) => Effect.sync(() => console.log(Array.from(chunk)))),
-  Effect.tapErrorCause(Effect.logErrorCause),
-  Effect.provideLayer(Logger.minimumLogLevel(Level.Debug))
+  Effect.forEachPar((n) => (n % 3 === 0 ? Effect.die("boom") : Effect.succeed(n + 1)))
 )
+  .flatMap((chunk) => Effect.sync(() => console.log(Array.from(chunk))))
+  .tapErrorCause((_) => Effect.logErrorCauseMessage("abc", _))
+  .provideLayer(Logger.minimumLogLevel(Level.Debug))
 
-Effect.unsafeFork(program)
+program.unsafeFork
